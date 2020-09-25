d3015cc9354c05190db2b3b01815c3f4e4ae0aa0
Ricky Bobby <richard.pajewski@gmail.com>
2020-09-25 13:12:54 -0500

    created more javascript functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,55 +5,90 @@ var questions = [
     { 
         q: 'Arrays in Javascript can be used to store _________.',
         c: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-        a: 4 
+        a: 'all of the above' 
     },
     {
         q: 'Commonly used data points do NOT include:',
         c: ['strings', 'booleans', 'alerts', 'numbers'],
-        a: 3
+        a: 'alerts'
     },
     {
         q: 'A very useful tool used during development and debugging for printing content to be debugger is:',
         c: ['javascript', 'terminal/bash', 'for loops', 'console.log'],
-        a: 4
+        a: 'console.log'
     },
     {
         q: 'The condition in an if/else statement is enclosed with _________.',
         c: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-        a: 2
+        a: 'curly brackets'
     },
     {
         q: 'String values must be enclosed within _________ when being assigned to variables.',
         c: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
-        a: 3
+        a: 'quotes'
     }
 ];
 
 // assign a variables to DOM objects
-var titleEl = document.querySelector("#title-questions");
-var buttonAnswerEl = document.querySelector('#button-answer');
+var titleEl = document.querySelector("#title");
+var answerEl = document.querySelector('#question-answer');
+var questionEl = document.querySelector('#questions');
+var pageContentEl = document.querySelector("#page-content");
 
 // challenge title and description
 var titlePage = function() {
     // opening title
     var openingTitle = document.createElement('h2');
-    openingTitle.className = 'title';
     openingTitle.textContent = 'Coding Quiz Challenge';
     titleEl.appendChild(openingTitle);
     // quiz description
     var openingText = document.createElement('p');
-    openingText.className = 'title';
     openingText.innerHTML = 'Try to answer the following code-related questions within the time limit. <br /> Keep in mind that incorrect answers will penalize your score/time <br /> by 10 seconds.';
     titleEl.appendChild(openingText);
     // start button
     var startButton = document.createElement('button');
-    startButton.className = 'start-button';
+    startButton.className = 'btn start-button';
     startButton.textContent = 'Start Quiz';
-    buttonAnswerEl.appendChild(startButton);
+    titleEl.appendChild(startButton);
+};
+
+// button function
+var taskButtonHandler = function(event) {
+    var targetEl = event.target;
+    // start button handler
+    if (targetEl.matches(".start-button")) {
+        startQuiz();
+    }
+    // submit button handler
+    else if (targetEl.matches(".submit-button")) {
+        submitScore();
+    }
+    // return to beginning handler
+    else if (targetEl.matches(".return-button")) {
+        returnHome();
+    }
+
+    // view high scores handler
+    else if (targetEl.matches(".high-score-button")) {
+        highScoreButton();
+    }
+};
+
+var startQuiz = function() {
+    // clear welcome page
+    welcomePageEl = document.getElementById('title');
+    welcomePageEl.setAttribute('class', 'hide');
+
+    //start timer
+    // timerStart();
+
+    // pull first question
+    for (i = 0; i < questions.length; i++) {
+        var question = questions[i];
+        questionEl.textContent = question.q;
+    }
 };
 
 titlePage();
 
-var runQuiz = function() {
-
-}
+pageContentEl.addEventListener("click", taskButtonHandler);
